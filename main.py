14525438f7b8a9224cfa5fc6ea0291757e521cf1
Areg Sargsyan <178701282+AregSPZ@users.noreply.github.com>
2025-04-17 09:34:03 +0400

    init the model

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer # import the loaders for a pretrained model and corresponding tokenizer
 
+# load DistilGPT-2 (a lightweight version of GPT-2)
+model_name = "distilgpt2"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+# init 
+topic = "French Revolution"
+init_prompt = f"Generate a quiz based on the topic: {topic}"
